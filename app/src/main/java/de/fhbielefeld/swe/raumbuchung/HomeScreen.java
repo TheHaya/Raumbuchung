bc4b47ed711b898187cf80d0c8d714ab8175b6df
thehaya <wong_alwin@hotmail.de>
2019-12-04 20:35:48 +0100

    HomeScreen GUI HomeScreen java/fragment MainActivity java

--- app/src/main/java/de/fhbielefeld/swe/raumbuchung/HomeScreen.java
+++ app/src/main/java/de/fhbielefeld/swe/raumbuchung/HomeScreen.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -20,12 +23,25 @@ public class HomeScreen extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home_screen, container, false);
+        ListView listView_home = view.findViewById(R.id.listView_home);
+        final MainActivity mainActivity = (MainActivity) getActivity();
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (mainActivity, android.R.layout.simple_list_item_1, mainActivity.arrayList);
+        listView_home.setAdapter(arrayAdapter);
+        listView_home.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mainActivity.deleteItem(position);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        return view;
     }
 
 }
--- app/src/main/java/de/fhbielefeld/swe/raumbuchung/MainActivity.java
+++ app/src/main/java/de/fhbielefeld/swe/raumbuchung/MainActivity.java
@@ -1,14 +1,24 @@
 package de.fhbielefeld.swe.raumbuchung;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.ArrayList;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> arrayList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addItem(String s) {
+        arrayList.add(s);
+    }
+
+    public void deleteItem(int position) {
+        arrayList.remove(position);
     }
 }
